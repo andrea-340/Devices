634ba328089f458870759b181aa3cd10e0a3721d
Andrea Altomare <164928731+andrea-340@users.noreply.github.com>
2024-05-16 00:48:17 +0200

    Add files via upload

--- js/file.js
+++ js/file.js
@@ -1,3 +1,4 @@
+
 navigator.geolocation.getCurrentPosition(position => {
     console.log('Latitude:', position.coords.latitude);
     console.log('Longitude:', position.coords.longitude);
@@ -10,5 +11,14 @@ Notification.requestPermission().then(permission => {
       new Notification('Hello, World!');
     }
 });
-  
-  
+document.addEventListener('DOMContentLoaded', (event) => {
+  // Chiede il nome dell'utente quando la pagina viene caricata
+  const userName = prompt('Per favore, inserisci il tuo nome:');
+
+  // Trova l'elemento con l'id 'user-name' e imposta il suo contenuto con il nome dell'utente
+  if (userName) {
+      document.getElementById('user-name').innerText = userName;
+  } else {
+      document.getElementById('user-name').innerText = 'Ospite';
+  }
+});
